22f93d764c3be1fa77fe3cff91ff158598daef3a
awdensmore <densmore@gmail.com>
2015-01-08 08:37:58 +0200

    Still working on battery class

--- mg-econ-main.py
+++ mg-econ-main.py
@@ -1,9 +1,10 @@
 #!/usr/bin env python
 
 #import numpy as np
+import matplotlib.pyplot as plt
 
 #dmnd_day = [[] for hr in range(0,24)]
-SIMHRS = 12
+#SIMHRS = 12
 dmnd_hr = []
 prod_hr = []
 
@@ -27,14 +28,9 @@ if len(dmnd_hr) != len(prod_hr):
 
 # Battery model
 class battery:
-    def __init__(self, size, soci=0.5, soc_min = 0.3, c_rate_d_max=0.05, c_rate_c_max=0.3, \
+    def __init__(self, size, soc_min = 0.3, c_rate_d_max=0.05, c_rate_c_max=0.3, \
                  cc_cv_trans_pt=0.8, eff_c=0.9, eff_d=0.9):
         self.size = size
-        self.soc = [0 for x in range(SIMHRS)] # initialize the state of charge array
-        self.soc_w = [0 for x in range(SIMHRS)]
-        self.c_mode = ["" for x in range(SIMHRS)] # bool, can be 'cc' or 'cv'
-        self.state = {"c_mode":"cc"} #"on" # bool, can be 'on' or 'off'
-        self.balance = [0 for x in range(SIMHRS)] # list containing the hourly difference between production and load
         self.__c_rate_c_max = c_rate_c_max
         self.__c_rate_d_max = c_rate_d_max
         self.__c_trans_pt = cc_cv_trans_pt
@@ -42,8 +38,15 @@ class battery:
 
     # Calculates the battery state of charge (soc) at every hour of the simulation period
     # Inputs: list of hourly solar production and load (watts)
-    # Returns:
-    def bat_soc(self, prod, ld):
+    # Returns: hourly soc, charging mode, state (on/off), and the difference b/w production and load
+    def bat_soc(self, prod, ld, soci):
+        simhrs = len(prod)
+        self.soc = [soci] + [0 for x in range(simhrs)] # initialize the state of charge array (%)
+        self.soc_w = [soci * self.size] + [0 for x in range(simhrs)] # soc in watts
+        self.c_mode = ["cc"] + ["" for x in range(simhrs)] # bool, can be 'cc' or 'cv'
+        self.state = ["ON"] + ["" for x in range(simhrs)]
+        self.balance = [0 for x in range(simhrs)] # list containing the hourly difference between production and load
+
         # Constant current charge mode
         # Inputs: battery soc (%) at beginning of hour, watts into battery
         # Returns list of battery soc at end of hour, any excess energy not used (if w_in exceeds c_rate_c_max)
@@ -83,36 +86,53 @@ class battery:
                 c_mode = "cc"
             return c_mode
 
-        for i in range(SIMHRS-1):
+        for i in range(simhrs):
             bal = prod[i] - ld[i]
             self.balance[i] = bal
             # Battery discharging
             if bal < 0:
                 if self.soc[i] <= self.__soc_min: # battery soc at or below min, disconnect battery
-                    self.state = "OFF"
                     self.soc[i+1] = self.soc[i]
                 #if abs(bal) > self.__c_rate_d_max:
                     # discharge rate exceeds max allowed, therefore load shedding
                 else: # discharge battery
                     rslt = __discharge(self.soc[i], bal)
                     self.soc[i+1] = rslt
-            # Battery charging
+            # Constant current battery charging
             elif __c_mode(self.soc[i]) == "cc":
                 rslt = __cc_charge(self.soc[i], bal)
                 self.soc[i+1] = rslt[0]
+                self.c_mode[i+1] = "cc"
+            # Constant voltage battery charging
             elif __c_mode(self.soc[i]) == "cv":
                 rslt = __cv_charge(self.soc[i], bal)
                 self.soc[i+1] = rslt[0]
+                self.c_mode[i+1] = "cv"
+            # Set battery state
+            if self.soc[i] >= self.__soc_min:
+                self.state[i+1] = "ON"
+            else:
+                self.state[i+1] = "OFF"
             self.soc_w[i+1] = self.soc[i+1] * self.size
+
+# DEMAND RESPONSE
+
 
 
 li_ion = battery(800)
-li_ion.bat_soc(prod_hr, dmnd_hr)
-print(prod_hr)
-print(dmnd_hr)
-print(li_ion.balance)
-print(li_ion.soc_w)
-print(li_ion.soc)
+li_ion.bat_soc(prod_hr, dmnd_hr, 0.5)
+print("Solar production:       " + str(prod_hr))
+print("Demand:                 " + str(dmnd_hr))
+print("Diff bw prod/load:      " + str(li_ion.balance))
+print("Battery state (w):      " + str(li_ion.soc_w[1:]))
+print("Battery SoC (%):        " + str(li_ion.soc[1:]))
+print("Battery state (on/off): " + str(li_ion.state[1:]))
+print("Battery charge mode:    " + str(li_ion.c_mode[1:]))
 
+
+# PLOTTING
+plt.plot(range(len(prod_hr)), prod_hr, range(len(dmnd_hr)), dmnd_hr)
+plt.plot(range(len(li_ion.soc_w[1:])), li_ion.soc_w[1:])
+plt.show()
 
 
---
+++ plot.py
@@ -0,0 +1,8 @@
+__author__ = 'adensmore'
+
+import matplotlib.pyplot as plt
+
+x = [range(0,5)]
+
+plt.plot(x, x, 'ro')
+plt.show()
