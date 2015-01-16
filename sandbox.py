b3912ad5b3eb8233f415f04f0d9ed2ee3e11c164
awdensmore <densmore@gmail.com>
2015-01-16 12:04:00 +0200

    Updated sv charging to be more flexible. Fixed minor bugs in sbat charging cycle. Needs testing.

--- sandbox.py
+++ sandbox.py
@@ -1,31 +1,21 @@
-import mg_econ_main as main
+import math
 
-t = 2
+def cv_charge(soc, c_trans_pt, w_in, t_add=1):
+    power = -0.5
+    size = 1000
+    # During cv charging the soc is assumed to approach 100% by m/(t^p)+b, where t is the time in hrs since
+    # crossing the cc/cv transition point, and p, m & b are constants. "hrs" is scaled from 1 (moment of transition)
+    # to 6 (5 hrs after transition), ie, it's assumed to take 5hrs to fully charge during cv mode.
+    m = (1 - c_trans_pt) / ((math.pow(6,power) - math.pow(1,power)))
+    b = 1 - (m * math.pow(6,power))
+    t = math.pow((m / (soc - b)),-1/float(power))
+    soc_max = m / math.pow((t+t_add),-power) + b
+    soc_win = soc + (w_in / float(size))
+    soc_new = min(soc_max, soc_win)
+    w_unused = max(0, (soc_win - soc_max) * size)
+    print(t)
+    print(soc_max)
+    print(soc_win)
+    print(w_unused)
 
-a = main.battery(5000, "s")
-b = main.battery(5000, "s")
-d = main.battery(3000, "s")
-c = main.battery(10000, "m")
-m = main.control([a,b,c,d])
-
-# Test 1 - charging
-if t == 1:
-    m.run([1000,1000,1000, 3000],[500,2000,0,2100],{1:[5,0.2],2:[10,0.1],3:[8,0.15],4:[1,0.1],5:[12,0.25],6:[3,.2]})
-    print(m.soc)
-    print(m.soc_w)
-    print(m.bal)
-
-# Test 2 - discharging
-if t == 2:
-    m.run([1000,1000,1000, 3000],[2500,3000,2000,2100],{1:[5,0.2],2:[10,0.1],3:[8,0.15],4:[1,0.1],5:[12,0.25],6:[3,.2]})
-    print(m.soc)
-    print(m.soc_w)
-    print(m.bal)
-
-# Test 3 - large data set
-if t == 3:
-    prod, demand = main.read_files("production.txt", "demand.txt")
-    m.run(prod,demand,{1:[5,0.2],2:[10,0.1],3:[8,0.15],4:[1,0.1],5:[12,0.25],6:[3,.2]})
-    print(m.soc)
-    print(m.soc_w)
-    print(m.bal)
+cv_charge(.899, 0.8, 200)
