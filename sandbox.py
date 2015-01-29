9b4dabbc3dee0843bbe2962581f4cddf8fa0cb4b
awdensmore <densmore@gmail.com>
2015-01-29 14:33:38 +0200

    Saved results to file. 'mgplot' graphs the results vs. baseline

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,12 @@
-import ttk
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels)
+red_patch = mpatches.Patch(marker="o", label="The red data")
+blue_patch = mpatches.Patch(color='blue', label='The blue data')
+ax.legend(handler = [red_patch, blue_patch])
+
+plt.show()
